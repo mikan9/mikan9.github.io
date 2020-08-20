111211906b9976376e442345a1efb6f15bcebd76
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2020-08-20 23:29:33 +0200

    Add nav button animation

--- js/index.js
+++ js/index.js
@@ -1,24 +1,45 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-const topButton = document.querySelector('.top-button');
+var topButton = $(".top-button");
+var lastId,
+    nav = $(".nav__list"),
+    navHeight = nav.outerHeight() + 15,
+    navItems = nav.find("a"),
+    scrollItems = navItems.map(function () {
+        var item = $($(this).attr("href"));
+        if (item.length) return item;
+    });
 
-navToggle.addEventListener('click', () => {
+$(".nav-toggle").click(() => {
     document.body.classList.toggle('nav-open');
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    });
+$(".nav__link").click(() => {
+    document.body.classList.remove('nav-open');
 });
 
-window.addEventListener("scroll", () => {
-    // pageYOffset >= 200 ? topButton.style.display = '' : topButton.style.display = 'none';
+$(window).scroll(() => {
     if (pageYOffset >= 200) {
-        topButton.classList.add('top-button--show');
-        topButton.classList.remove('top-button--hide');
+        topButton.addClass('top-button--show');
+        topButton.removeClass('top-button--hide');
     } else {
-        topButton.classList.add('top-button--hide');
-        topButton.classList.remove('top-button--show');
+        topButton.addClass('top-button--hide');
+        topButton.removeClass('top-button--show');
+    }
+
+    var fromTop = $(this).scrollTop() + navHeight;
+
+    var cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop) {
+            return this;
+        }
+    });
+
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        navItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
     }
 });
